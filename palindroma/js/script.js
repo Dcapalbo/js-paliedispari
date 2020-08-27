80c6549968c88b4193d24cd2ff1eadb1ec1277f3
Domenico Capalbo <capalbodomenico@gmail.com>
2020-08-27 16:29:45 +0200

    inserimento-funzione-palindromo
    js-paliesipari

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,15 +4,30 @@
 
 //1. ask to the user to insert a word
 var wordUser = prompt("insert a word");
-
-//2. create an array and push the wordUser inside of it
-
-var wordArray = [];
-wordArray.push(wordUser);
-const reversed = wordArray.reverse();
+var control = true;
 
 
-console.log(wordUser);
+//2. crete a function usefull to control
+
+function checkingPalindrom(wordUser) {
+  for (var i = 0; i < wordUser.length; i++) {
+    var leftWord = wordUser[i];
+    var rightWord = wordUser.charAt(wordUser.length -i -1);
+    if (leftWord !== rightWord) {
+      control = false;
+    }
+  } return control;
+}
+
+if (control) {
+  console.log("this is a palindrom");
+} else {
+  console.log("this is not a palindrom");
+}
+
+
+
+
 // //3. create a function usefull to find if a word is palindrom or it is not
 // function checkPalindrom (str) {
 //   if (wordUser) {
